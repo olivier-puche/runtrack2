f07ae72cf697a39768c74de8a86d995efd192df0
olivier-puche <71874455+olivier-puche@users.noreply.github.com>
2020-11-10 11:34:36 +0100

    matin

--- jour07/job04/index.php
+++ jour07/job04/index.php
@@ -1,29 +1,3 @@
 <?php
-session_start();
-
-if (isset($_POST["connecte"])) || (isset($_POS["deco"]))
-{
-    setcookie('prenom');
-
-}
-
-echo $_POST;
-
-else
-{
-    setcookie('nbvisites', $_COOKIE['prenom'];
-
-}
-
 
 ?>
-
-<!DOCTYPE html>
-
-<form action="index.php" method="post">
-    <input type="text" name="prenom" />
-    <button name="connexion" type="submit" value="connecte">valider</button>
-    <button name="deco" value="deco">reset</button>
-</form>
-</html>
-
